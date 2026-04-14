7f21e5de594a9fd624cdee73052fca532d0c2f93
Miszuszix <adels123456@gmail.com>
2026-04-14 19:29:18 +0200

    fix ip problem

--- ControllerApp/network_manager.py
+++ ControllerApp/network_manager.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
 import urllib.error
+import socket  # DODANY IMPORT
 from PySide6.QtCore import QThread, Signal
 
 class NetworkWorker(QThread):
@@ -16,14 +17,18 @@ class NetworkWorker(QThread):
 
     def run(self):
         for target in self.targets:
-            ip_address = target.get("ip")
-            url = f"http://{ip_address}:5000/{self.action}"
+            original_target_ip = target.get("ip")
+            try:
+                resolved_ip = socket.gethostbyname(original_target_ip)
+            except Exception:
+                resolved_ip = original_target_ip
+
+            url = f"http://{resolved_ip}:5000/{self.action}"
             headers = {"Content-Type": "application/json"}
 
             if self.token:
                 headers["Authorization"] = f"Bearer {self.token}"
 
-            # POPRAWKA: Upewniamy się, że zawsze wysyłamy poprawny JSON, nawet jeśli payload to pusty słownik {}
             safe_payload = self.payload if self.payload is not None else {}
             request_data = json.dumps(safe_payload).encode('utf-8')
 
@@ -31,15 +36,15 @@ class NetworkWorker(QThread):
                 request = urllib.request.Request(url, data=request_data, headers=headers, method="POST")
                 with urllib.request.urlopen(request, timeout=10) as response:
                     response_body = json.loads(response.read().decode('utf-8'))
-                    self.progress_signal.emit(ip_address, True, response_body.get("msg", "OK"))
+                    self.progress_signal.emit(original_target_ip, True, response_body.get("msg", "OK"))
             except urllib.error.HTTPError as http_error:
                 try:
                     error_body = json.loads(http_error.read().decode('utf-8'))
-                    self.progress_signal.emit(ip_address, False, error_body.get("msg", str(http_error)))
+                    self.progress_signal.emit(original_target_ip, False, error_body.get("msg", str(http_error)))
                 except Exception:
-                    self.progress_signal.emit(ip_address, False, str(http_error))
+                    self.progress_signal.emit(original_target_ip, False, str(http_error))
             except Exception as exception:
-                self.progress_signal.emit(ip_address, False, str(exception))
+                self.progress_signal.emit(original_target_ip, False, str(exception))
 
         self.finished_signal.emit()
 
